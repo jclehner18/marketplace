898dfc2196c1bc7180f4f4a6640309d0e9c8249b
Jack Lehner <jacklehner44@yahoo.com>
2023-07-17 19:42:27 -0400

    fixed error with geolocation. images being uploaded to firebase storage

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -4,6 +4,9 @@ import {getAuth, onAuthStateChanged} from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 import Spinner from '../components/Spinner'
 import {toast} from 'react-toastify'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import db from '../firebase.config'
+import {v4 as uuidv4} from 'uuid'
 
 function CreateListing() {
 
@@ -29,7 +32,7 @@ function CreateListing() {
 
     const auth = getAuth()
     const navigate = useNavigate()
-    const isMounted = useRef(true)
+    //const isMounted = useRef(true)
     
     // useEffect(() => {
     //     if(isMounted) {
@@ -83,13 +86,10 @@ function CreateListing() {
         let location
 
         if(geolocationEnabled) {
-            
-            var requestOptions = {
-                method: 'GET'
-            }
 
-            const response = await fetch(`https://api.geoapify.com/v1/geocode/search?text=${address}&apiKey=${process.env.REACT_APP_GEOCODE_APPI_KEY}`)
+            const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`)
             const data = await response.json()
+
             geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
             geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
 
@@ -104,8 +104,59 @@ function CreateListing() {
         } else {
             geolocation.lat = latitude
             geolocation.lng = longitude
-            location = address
+            //location = address
         }
+
+        //store images in firebase
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage()
+                const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+
+                const storageRef = ref(storage, 'images/' + fileName)
+
+                const uploadTask = uploadBytesResumable(storageRef, image);
+
+
+                uploadTask.on('state_changed', 
+                    (snapshot) => {
+                        
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                        switch (snapshot.state) {
+                        case 'paused':
+                            console.log('Upload is paused');
+                            break;
+                        case 'running':
+                            console.log('Upload is running');
+                            break;
+                        default:
+                            break
+                        }
+                    }, 
+                    (error) => {
+                        reject(error)
+                    }, 
+                    () => {
+                        // Handle successful uploads on complete
+                        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                            resolve(downloadURL);
+                        });
+                    }
+                );
+            })
+        }
+
+        const imgUrls = await Promise.all(
+            [...images].map((image) => storeImage(image))
+        ).catch(() => {
+            setLoading(false) 
+            toast.error('Images not uploaded')
+            return
+        })
+
+        console.log(imgUrls)
 
         setLoading(false)
     }
